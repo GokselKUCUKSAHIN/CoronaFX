52350d857798a4963a312e8adf583b178c9327ac
Jellybeanci <gokselkucuksahin@gmail.com>
2020-06-06 23:44:16 +0300

    record edited.

--- src/Record.java
+++ src/Record.java
@@ -1,5 +1,15 @@
 public class Record
 {
     private String date;
-   // private int
+    private int day;
+    private int month;
+    private int year;
+
+    public Record(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
 }
