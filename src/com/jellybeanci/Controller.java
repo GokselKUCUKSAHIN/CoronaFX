67d804e979eca60b0552abdbe554934a564e63bf
Jellybeanci <gokselkucuksahin@gmail.com>
2020-06-11 21:04:54 +0300

    Table View added.

--- src/com/jellybeanci/Controller.java
+++ src/com/jellybeanci/Controller.java
@@ -3,6 +3,8 @@ package com.jellybeanci;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,9 +13,6 @@ import static com.jellybeanci.GetData.readFromWeb;
 
 public class Controller
 {
-
-    @FXML
-    public TableView tableView;
 
     @FXML
     private TextField textBox;
@@ -39,11 +38,14 @@ public class Controller
                     e.printStackTrace();
                 }
                 Record.parse(contList);
+                /*
                 for (Country country : Country.countries.values())
                 {
                     System.out.println(country.toString());
                 }
+                */
                 btnGetData.setDisable(false);
+                tableView.setItems(Country.getObservableList());
             }
         };
         new Thread(r).start();
@@ -61,6 +63,49 @@ public class Controller
                 btnGetData.setText(Math.random() + "");
             }
         });
+    }
+
+    //
+    // Table View
+    //
+    @FXML
+    private TableView<Country> tableView;
+
+    @FXML
+    private TableColumn<Country, String> countryName;
+
+    @FXML
+    private TableColumn<Country, Integer> totalCases;
+
+    @FXML
+    private TableColumn<Country, Integer> newCases;
+
+    @FXML
+    private TableColumn<Country, Integer> totalDeaths;
+
+    @FXML
+    private TableColumn<Country, Integer> newDeaths;
+
+    @FXML
+    private TableColumn<Country, Integer> population;
+
+    @FXML
+    private TableColumn<Country, Double> mortality;
+
+    @FXML
+    private TableColumn<Country, Double> attackRate;
+
+    @FXML
+    private void initialize()
+    {
+        countryName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        totalCases.setCellValueFactory(new PropertyValueFactory<>("totalCases"));
+        newCases.setCellValueFactory(new PropertyValueFactory<>("newCases"));
+        totalDeaths.setCellValueFactory(new PropertyValueFactory<>("totalDeaths"));
+        newDeaths.setCellValueFactory(new PropertyValueFactory<>("newDeaths"));
+        population.setCellValueFactory(new PropertyValueFactory<>("population"));
+        mortality.setCellValueFactory(new PropertyValueFactory<>("mortality"));
+        attackRate.setCellValueFactory(new PropertyValueFactory<>("attackRate"));
     }
 }
 
