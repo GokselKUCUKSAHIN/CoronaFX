dbcafbc8d2ff7861972a83258b84010de617b516
Jellybeanci <gokselkucuksahin@gmail.com>
2020-06-11 14:28:56 +0300

    Multi Thread.

--- src/com/jellybeanci/Controller.java
+++ src/com/jellybeanci/Controller.java
@@ -1,7 +1,13 @@
 package com.jellybeanci;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.jellybeanci.GetData.readFromWeb;
 
 public class Controller
 {
@@ -18,6 +24,43 @@ public class Controller
     @FXML
     protected void onGetDataClick()
     {
-        btnGetData.setText(Math.random() + "");
+        btnGetData.setDisable(true);
+        Runnable r = new Runnable()
+        {
+            public void run()
+            {
+                ArrayList<String> contList = null;
+                try
+                {
+                    contList = GetData.readFromWeb("https://opendata.ecdc.europa.eu/covid19/casedistribution/xml/");
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                Record.parse(contList);
+                for (Country country : Country.countries.values())
+                {
+                    System.out.println(country.toString());
+                }
+                btnGetData.setDisable(false);
+            }
+        };
+        new Thread(r).start();
+    }
+
+    @FXML
+    protected void threadTestButton()
+    {
+        Platform.runLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // Update UI here.
+                btnGetData.setText(Math.random() + "");
+            }
+        });
     }
 }
+
